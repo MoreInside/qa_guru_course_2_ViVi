8358fbee347795665d77c11140a49b412a467215
MoreInside <cyberbelka.msk@gmail.com>
2020-08-02 22:33:53 +0300

    new commit

---
+++ src/test/java/tests/YandexTest.java
@@ -0,0 +1,17 @@
+package tests;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class YandexTest {
+    @Test
+    void searchTest() {
+       open("https://yandex.ru");
+        $(byClassName("input__control")).setValue("Kung Fu Panda").pressEnter();
+        $("html").shouldHave(text("Kung Fu Panda"));
+    }
+}
