6dde218f2cf864a327d984eb9261be4d3063ffa7
MoreInside <cyberbelka.msk@gmail.com>
2020-08-05 20:10:22 +0300

    HW_3

--- src/test/java/tests/LamodaSearch_HW_2.java
+++ src/test/java/tests/LamodaSearch_HW_2.java
@@ -11,12 +11,14 @@ import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class LamodaSearch {
+public class LamodaSearch_HW_2 {
     @Test
     void LamodaSearchTest() {
         // Открыть lamoda.ru
         open("https://lamoda.ru");
+
         $(byText("Женщинам")).click();
+
         $("body").shouldHave(text("Женщинам"));
 
 }
