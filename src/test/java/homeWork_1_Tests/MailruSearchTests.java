c4854f505093fe90a8dbb18dba0260145eab24a5
MoreInside <cyberbelka.msk@gmail.com>
2020-08-19 00:17:30 +0300

    refresh name commit

--- src/test/java/homeWork_1_Tests/MailruSearchTests.java
+++ src/test/java/homeWork_1_Tests/MailruSearchTests.java
@@ -1,21 +1,24 @@
 package homeWork_1_Tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byName;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class MailruSearchTests {
     @Test
+    @DisplayName("MailRuSearchTest")
     void selenideSearchTest() {
         // Открыть mail.ru
         open("https://mail.ru");
-        $("h1 div");
 
-        // Ввести Selenide в поиск
-       // $(byName("q")).setValue("Selenide").pressEnter();
+         //Ввести Selenide в поиск
+       $(byName("q")).setValue("Selenide").pressEnter();
 
         // Проверить, что Selenide появился в результатах поиска
-       // $("html").shouldHave(text("ru.selenide.org"));
+       $("html").shouldHave(text("ru.selenide.org"));
     }
 }
